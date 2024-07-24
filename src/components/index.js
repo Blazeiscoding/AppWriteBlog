16c59cafbfa46b7179ce00324ae32245581b8422
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-07-24 11:24:06 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './Container/Container'
+import Logo from "./Logo";
+import Logoutbtn from "./Header/Logoutbtn";
+import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Logoutbtn,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Button,
+    Input,
+    Select,
+}
